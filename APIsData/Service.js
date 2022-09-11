16a302f7cbba3c0d7f957a00f171c28a3a73ac8b
Sivaji777 <86228129+Sivaji777@users.noreply.github.com>
2022-09-11 15:45:15 +0530

    Update Service.js

--- APIsData/Service.js
+++ APIsData/Service.js
@@ -1,3 +1,60 @@
+/* using fetch method-1 */
+     const token = 'fpyzw76umprjq7cpogb22tnglr7e644neoxp1727'
+     fetch('https://api.json-generator.com/templates/Ekx4iiPPM8Ee/data', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res, '11111111');
+            })
+            .catch((error) => {
+            });
+
+/* using fetch method-2 */
+  const token = 'fpyzw76umprjq7cpogb22tnglr7e644neoxp1727'
+  const payload = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        fetch( 'https://api.json-generator.com/templates/Ekx4iiPPM8Ee/data',payload)
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res, '11111111');
+            })
+            .catch((error) => {
+            });
+
+/* using axios method-1 */
+    const token = 'fpyzw76umprjq7cpogb22tnglr7e644neoxp1727'
+    axios.get('https://api.json-generator.com/templates/Ekx4iiPPM8Ee/data', {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        }).then((res) => {
+            setData(res.data.patients)
+        }).catch((err) => {
+            console.log(err, '111');
+        })
+
+/* using axios method-2 */
+    const token = 'fpyzw76umprjq7cpogb22tnglr7e644neoxp1727'
+    const payload = {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        axios.get('https://api.json-generator.com/templates/Ekx4iiPPM8Ee/data', payload).then((res) => {
+            setData(res.data.patients)
+        }).catch((err) => {
+            console.log(err, '111');
+        })
+
+
 /* axios library */ 
 import axios from 'axios'
 /* base URL */
